4a06fb4392f93a8315f69c300a2f9094083e46a5
wolframhwang <hzw94@naver.com>
2021-12-13 00:02:07 +0900

    Initial Commit

---
+++ igg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  igg
+//
+//  Created by 황지웅 on 2021/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
